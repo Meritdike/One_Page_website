2fe7905b49eaf2cbfcb5b940aa98c6bec7576928
Meritdike <dike.merit@yahoo.com>
2022-05-17 23:27:30 +0100

    ONE PAGE WEBSITE HOMEPAGE

--- js/script.js
+++ js/script.js
@@ -7,3 +7,50 @@ var $grid = $('#portfolio-items').isotope({
       var filterValue = $(this).attr('data-filter');
       $grid.isotope({ filter: filterValue });
 });
+
+const btns = document.querySelectorAll (".portfolio-button");
+const portfolioItems = document.querySelectorAll (".portfolio-image-section");
+
+
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener ("click", (e) => {
+//     e.preventDefault();
+    
+//     const filter = e.target.dataset.filter; 
+     
+//     portfolioItems.forEach((portfolioItem)=> {
+//       if (filter=="all") {
+//         portfolioItem.style.display ="block"
+//       } else {
+//         if (portfolioItem.classList.contains(filter)) {
+//         portfolioItem.style.display= "block"
+//       } else {
+//         portfolioItem.style.display= "none"
+//       }
+//       }
+//     })
+//   })
+// }
+
+btns.forEach(btn => {
+  btn.addEventListener ('click', function(){
+    btns.forEach(btn => btn.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
+
+let valueDisplays = document.querySelectorAll(".counter");
+let interval = 6000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval( function() {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
