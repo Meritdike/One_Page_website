a647e8df248a9296a2183e4c0b0217dc79509d9d
Meritdike <dike.merit@yahoo.com>
2022-05-18 00:06:40 +0100

    ONE PAGE WEBSITE HOMEPAGE

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ btns.forEach(btn => {
 });
 
 let valueDisplays = document.querySelectorAll(".counter");
-let interval = 6000;
+let interval = 3000;
 
 valueDisplays.forEach((valueDisplay) => {
   let startValue = 0;
